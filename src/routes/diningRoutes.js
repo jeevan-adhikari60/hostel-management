b16dfb85d6eea16db9f15f07b97249a0ffa325ae
Jeevan Adhikari <jeevanbhai6676@gmail.com>
2026-02-21 22:27:47 +0545

    nothing just one comment

--- src/routes/diningRoutes.js
+++ src/routes/diningRoutes.js
@@ -11,16 +11,17 @@ import { protectedRoutes } from '../middleware/protectedroutes.js';
 import { restrictTo } from "../middleware/restriction.js";
 
 const router = express.Router({ mergeParams: true });
-
+// nologin required routes
 router.route("/meals").get(getAllMeals);
 router.route("/meals/weekly-schedule").get(getWeeklySchedule);
 router.route("/meals/:day").get(getMealsByDay);
 
 router.use(protectedRoutes); 
+//now login required routes
 
-router.route("/meals").post(createMeal);
+router.route("/meals").post(createMeal);//student and admin can create meal
 router.route("/meals/:day/:mealType")
-  .put(restrictTo("student"), updateMeal)
+  .put(restrictTo("student"), updateMeal)//only admin can update and delete meal
   .delete(restrictTo("student"), deleteMeal);
 
 export default router;
